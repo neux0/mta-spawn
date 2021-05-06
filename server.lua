6bf6f159897af4bddc1b53734c59286205b1bdaa
neux0 <69310876+neux0@users.noreply.github.com>
2021-05-06 05:38:39 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,24 +2,25 @@
 	local joinX = 0.0
 	local joinY = 0.0
 	local joinZ = 0.0
-	local joinMessage = "welcome dude"
+	local joinMessage = "welcome dude :)"
 	local joinSkin = 0
 	local spawnX = 0.0
 	local spawnY = 0.0
 	local spawnZ = 0.0
 	local spawnSkin = 0
 
+-- when player join
 function spawnOnJoin()
 	spawnPlayer(source, joinX, joinY, joinZ, 0 , joinSkin)
 	fadeCamera(source, true)
 	setCameraTarget(source, source)
 	outputChatBox(joinMessage, source)
 end
+addEventHandler("onPlayerJoin", getRootElement(), spawnOnJoin)
 
+-- when player die
 function spawnOnDead(ammo, killer, weapon, bodypart)
-	setTimer(spawnPlayer, 3000, 1, source, spawnX, spawnY, spawnZ, 0, spawnSkin)
+	setTimer(spawnPlayer, 4000, 1, source, spawnX, spawnY, spawnZ, 0, spawnSkin)
 	setCameraTarget(source, source)
 end
-
-addEventHandler("onPlayerJoin", getRootElement(), spawnOnJoin)
 addEventHandler("onPlayerWasted", getRootElement(), spawnOnDead)
